2c510ccb875eae7173ebd023b4793eccfb64e4d5
yozhik333 <repeinik333@gmail.com>
2020-02-21 09:01:32 +0600

    333dd3d3

--- app/js/common.js
+++ app/js/common.js
@@ -4,16 +4,34 @@ $(function() {
 });
 
 var swiper = new Swiper(".swiper-container", {
-  spaceBetween: 70,
-	effect: "slide",
+	spaceBetween: 70,
+	effect: "fade",
 	width: 543,
-  speed: 900,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
+	speed: 900,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	},
+	breakpoints: {
+		'@0.00': {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		'@0.75': {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		'@1.00': {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+		'@1.50': {
+			slidesPerView: 4,
+			spaceBetween: 50,
+		},
+	}
 });
