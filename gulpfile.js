ae08d54e0515bbe45d7f34bc836710a24cb80589
yozhik333 <repeinik333@gmail.com>
2020-02-25 23:01:40 +0600

    index

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('browser-sync', function() {
 		notify: false,
 		// open: false,
 		// online: false, // Work Offline Without Internet Connection
-		tunnel: true, tunnel: "orangetrading", // Demonstration page: http://projectname.localtunnel.me
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	})
 });
 
